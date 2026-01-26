3e83c2935d8418505dd7d9ff2ad6c97929871a27
Hitesh Kumar <55690134+HiteshBucky@users.noreply.github.com>
2026-01-26 16:28:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3000;
 
-app.get('/', (req, res) => {
+const PORT = Number(process.env.PORT) || '3000';
+
+app.get('/', async (req, res) => {
+
+  const user = await getUser();
+
+  res.send(`Hello ${user.name}`);
   res.send('Hello World');
 });
 
+const getUser = () => {
+  if (Math.random() > 0.5) {
+    return Promise.resolve({ name: 'World' });
+  }
+};
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
+
+Promise.reject('Unexpected rejection');
